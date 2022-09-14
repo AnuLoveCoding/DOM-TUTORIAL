c6a89bb72329f388bde9ce1f2df5cd130a96a25d
anuLoveCoading <anusrivas07@gmail.com>
2022-09-14 11:20:23 +0530

    aplhabet check

--- stinrg.js
+++ stinrg.js
@@ -14,13 +14,41 @@
 // }
 // console.log(obj);
 
-let arr=[7,2,7,7,3,4,2];
-let poc={};
-for (let j=0;j<=arr.length-1;j++){
-    if(poc[arr[j]]==undefined){;
-       poc[arr[j]]=1;
-   }else{
-    poc[arr[j]]++;
-   }
+// let arr=[7,2,7,7,3,4,2];
+// let poc={};
+// for (let j=0;j<=arr.length-1;j++){
+//     if(poc[arr[j]]==undefined){;
+//        poc[arr[j]]=1;
+//    }else{
+//     poc[arr[j]]++;
+//    }
+// }
+// console.log(poc);
+
+
+// find the sum of elements whose occurance is 2;
+
+
+let arr=[1,1,3,4,5,5,6,7,8,8];
+let obj={};
+
+
+for(let i=0;i<=arr.length-1;i++){
+    if(obj[arr[i]]==undefined){
+        obj[arr[i]]=1;
+
+    }else{
+        obj[arr[i]]++;
+    }
 }
-console.log(poc);
+console.log(obj);
+
+
+let sum=0;
+for(let key in obj){
+    if(obj[key==2]){
+        sum+=key;
+        
+    }
+}
+console.log(sum);
