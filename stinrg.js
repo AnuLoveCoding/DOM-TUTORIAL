5b6f0c06ffac914b59b111226ae49a55031eb8c5
anuLoveCoading <anusrivas07@gmail.com>
2022-09-14 10:24:48 +0530

    alphabet checking

--- stinrg.js
+++ stinrg.js
@@ -1,8 +1,12 @@
-let str ="Anurag";
+let str ="AaAnurag";
 let obj={};
 
+
+
+
 for (let i=0;i<=str.length-1;i++){
-    if(obj[str[i]]==undefined){
+    if(obj[str[i]]==undefined)//here checkimg value not present
+    {
         obj[str[i]]=1
     }else{
         obj[str[i]]++
