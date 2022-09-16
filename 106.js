0da39adcf7b10e20951216685bcd802fb3954e23
anuLoveCoading <anusrivas07@gmail.com>
2022-09-16 14:17:15 +0530

    Dom Tutorial

---
+++ 106.js
@@ -0,0 +1,6 @@
+// change the styles of element;
+
+const mainHeading= document.querySelector("div.headline h2");
+console.log(mainHeading.style);
+mainHeading.style.backgroundColor ="brown";
+mainHeading.style.border="5px solid green"
