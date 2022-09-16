d621177c0668377d14b5b4e5dcef1f726baafbaf
anuLoveCoading <anusrivas07@gmail.com>
2022-09-16 15:42:17 +0530

    Dom Tutorial

---
+++ 108.js
@@ -0,0 +1,14 @@
+// get multiple elements using getElements by class name;
+
+
+
+
+
+
+// const navItems = document.getElementsByClassName("nav-item");// HTML collection;
+// console.log(typeof navItems[0]);
+
+
+// get multiple elements items using querySelectorAll;
+// const navItems= document.querySelectorAll(".nav-item");
+// console.log(navItems[1]);
