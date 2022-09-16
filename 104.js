c1397e905204119a81ede7743da5e6bf0a126013
anuLoveCoading <anusrivas07@gmail.com>
2022-09-16 14:04:37 +0530

    Dom Tutorial

--- 104.js
+++ 104.js
@@ -2,7 +2,7 @@
 
 
 // const mainHeading = document.getElementById("main-heading");
-// const mainHeading = document.querySelector("#main-heading");
-// const header= document.querySelector(".header");
+const mainHeading = document.querySelector("#main-heading");
+const header= document.querySelector(".header");
 const navItems =document.querySelectorAll(".nav-item");
 console.log(navItems);
