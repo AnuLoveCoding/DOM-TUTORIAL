887543ed1f55d00936f1eb4df841837a03b04773
anuLoveCoading <anusrivas07@gmail.com>
2022-09-16 13:50:04 +0530

    Dom Tutorial

---
+++ 104.js
@@ -0,0 +1,8 @@
+// select element using query selector;
+
+
+// const mainHeading = document.getElementById("main-heading");
+// const mainHeading = document.querySelector("#main-heading");
+// const header= document.querySelector(".header");
+const navItems =document.querySelectorAll(".nav-item");
+console.log(navItems);
