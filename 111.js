905efc1347349cd811f92c615295562a945da5a3
anuLoveCoading <anusrivas07@gmail.com>
2022-09-17 16:42:45 +0530

    Dom Tutorial

---
+++ 111.js
@@ -0,0 +1,11 @@
+// const h1=document.querySelector("h1");
+// const body= h1.parentNode.parentNode;
+// body.style.color ="#efefef";
+// bosy.style.backgroundColor="#333";
+
+// const body = document.body;
+// console.log(body);
+
+
+const head = document.querySelector("head");
+console.log(head);
