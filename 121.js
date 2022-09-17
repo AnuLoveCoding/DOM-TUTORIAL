5e9aaa6f6de657c619fd16af145a2cb70701c448
anuLoveCoading <anusrivas07@gmail.com>
2022-09-17 18:41:52 +0530

    Dom tutorial

---
+++ 121.js
@@ -0,0 +1,18 @@
+
+// this keyword;
+
+
+const btn = document.querySelector(".btn-headline");
+
+btn.addEventListener("click",()=>{
+    console.log("arrow function!!!!!!!!!!!!!!");
+    console.log("value of this");
+    console.log(this);
+});
+
+
+// function myfunc(){
+    
+// }
+// myfunc();
+// btn.addEventListener("click");
