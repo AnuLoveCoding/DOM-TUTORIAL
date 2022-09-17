5ffea46bc41843323fa3f40b42b52d6ca2a22cd5
anuLoveCoading <anusrivas07@gmail.com>
2022-09-17 19:30:37 +0530

    Dom Tutorial

---
+++ 122.js
@@ -0,0 +1,7 @@
+// console.log("hello Anurag");
+
+ const firstButton =document.querySelectorAll("button");
+ console.log(firstButton)
+//  firstButton.addEventListener("click",function(){
+//     console.log("You click me!!!")
+//  });
